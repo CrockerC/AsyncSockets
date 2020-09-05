cf751f8cfb96bcd60e7140c5b8a7cc11e8dc8919
Christopher Crocker <66031632+CrockerC@users.noreply.github.com>
2020-09-05 14:36:08 -0400

    removed code that had been commented out

--- AsyncSockets.py
+++ AsyncSockets.py
@@ -11,14 +11,11 @@ class multipleListens:
 
         self.ret = threading.Semaphore()  # semaphore to protect self.value
         self.got = threading.Event()  # signal for self.loop to yeild the data in self.value
-        # self.vSafe = threading.Event()  # signal to self.listen that it is safe to write to self.value
         self.value = []
 
         self.close = threading.Event()
         self.exception = None
-
-        # self.vSafe.set()
-
+        
         self.updateListens()
 
     # loop is meant to be run immediately after the init it called, ie:
@@ -30,7 +27,6 @@ class multipleListens:
         for sid, sock in enumerate(self.socks):
             if sock not in self.listens:
                 self.listens.update({sock: threading.Thread(target=self.listen, args=(sock, sid)).start()})
-                # print("added, ", sock)
 
     def loop(self):
         while True:
@@ -51,16 +47,12 @@ class multipleListens:
             self.ret.acquire()  # acquire the semaphore
 
             yield self.value.pop(0)  # yield the data to the caller
-            # self.value = None  # clear the data
-            # self.vSafe.set()  # signal that it is now safe to write to self.value again
 
             if not self.value:
                 self.got.clear()  # signal that there is no value in self.value
 
             self.ret.release()  # release the semaphore
 
-    # todo, handle the loss of a sock
-    # todo, any sock other than 0 can be lost without terminating the connection
     def listen(self, sock, sid):
         while True:
             if self.close.isSet():
@@ -74,10 +66,8 @@ class multipleListens:
                     self.close.set()
                     return
 
-                # self.vSafe.wait()  # wait for self.value to be None
                 self.ret.acquire()  # acquire the semaphore
                 self.value.append((data, speed, sid))  # write the data to self.value
-                # self.vSafe.clear()  # signal that it is no longer safe to modify self.value
 
             else:
                 try:
@@ -87,10 +77,8 @@ class multipleListens:
                     self.close.set()
                     return
 
-                # self.vSafe.wait()
                 self.ret.acquire()
                 self.value.append((data, 0, sid))
-                # self.vSafe.clear()
 
             self.got.set()  # signal that we can now give the value to the caller of self.loop
             self.ret.release()  # release the semaphore
